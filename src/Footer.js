e52a2577fe5ff4425ab0b29488a5be0c4d918f5b
SteRap <92691510+SteRap@users.noreply.github.com>
2022-01-02 23:19:59 +0100

    adding social media links

--- src/Footer.js
+++ src/Footer.js
@@ -8,12 +8,15 @@ function Footer() {
       </a>
       <ul className="social-list">
         <li className="social-list__item">
-          <a className="social-list__link" href="https://linkedin.com">
+          <a
+            className="social-list__link"
+            href="https://www.linkedin.com/in/s-rapino"
+          >
             <i className="fab fa-linkedin"></i>
           </a>
         </li>
         <li className="social-list__item">
-          <a className="social-list__link" href="https://github.com">
+          <a className="social-list__link" href="https://github.com/SteRap">
             <i className="fab fa-github"></i>
           </a>
         </li>
