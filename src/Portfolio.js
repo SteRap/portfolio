c614a47be79844850d339e328e647a8c76012351
SteRap <92691510+SteRap@users.noreply.github.com>
2022-01-02 17:44:21 +0100

    add new project page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,11 @@
 import img0 from "./Media/Project/quizzical.png";
-import img0Pres from "./Media/Project/quizzical.png";
+import img0Pres from "./Media/Project/quizzical_presentation.png";
 import img1 from "./Media/Project/save_the_meal.png";
 import img1Pres from "./Media/Project/save_the_meal_presentation.png";
 import img2 from "./Media/Project/smart_brain.png";
 import img2Pres from "./Media/Project/smart_brain_presentation.png";
 import img3 from "./Media/Project/space_tourism.png";
-import img3Pres from "./Media/Project/quizzical.png";
+import img3Pres from "./Media/Project/space_tourism_presentation.png";
 import img4 from "./Media/Project/robofriends.png";
 import img4Pres from "./Media/Project/robofriends_presentation.png";
 // import img6 from "./Media/Project/project_6.png";
@@ -16,27 +16,32 @@ const Portfolio = [
   {
     id: 0,
     img: img0,
-    presentation: "",
+    presentation: img0Pres,
+    link: "https://quizzical-game.herokuapp.com",
   },
   {
     id: 1,
     img: img1,
     presentation: img1Pres,
+    link: "",
   },
   {
     id: 2,
     img: img2,
     presentation: img2Pres,
+    link: "",
   },
   {
     id: 3,
     img: img3,
-    presentation: "",
+    presentation: img3Pres,
+    link: "https://sterap.github.io/space-tourism-website/",
   },
   {
     id: 4,
     img: img4,
     presentation: img4Pres,
+    link: "",
   },
   // {
   //   id: 5,
